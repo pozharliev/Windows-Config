ed392945476ed4041713ed546f87fd9786933c4c
Atanas Pozharliev <abpozharliev19@codingburgas.bg>
2021-02-04 21:14:39 +0200

    Add vscode and windows terminal settings

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module posh-git
+Set-Theme Paradox
+$ThemeSettings.Colors.GitLocalChangesColor = [ConsoleColor]::DarkGreen
+$ThemeSettings.Colors.PromptBackgroundColor = [ConsoleColor]::DarkBlue
