3f20019275b4253615a1966f76e78a00e94ffa84
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-09-25 04:10:01 +0300

    Change file structure Change powershell profile Add powershell theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,3 @@
-Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module posh-git
-Set-Theme Paradox
-$ThemeSettings.Colors.GitLocalChangesColor = [ConsoleColor]::DarkGreen
-$ThemeSettings.Colors.PromptBackgroundColor = [ConsoleColor]::DarkBlue
+Set-PoshPrompt -Theme theme
